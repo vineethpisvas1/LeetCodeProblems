ce29ab522c95ba44d2b46061f893abd0f3637e88
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-05-11 23:21:37 +0530

    Added solution to #657 Robot Return To Origin

---
+++ Easy/RobotReturnToOrigin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0, y=0;
+        for(char c : moves.toCharArray()) {
+            switch(c) {
+                case 'U':
+                    y++;break;
+                case 'D':
+                    y--;break;
+                case 'R':
+                    x++;break;
+                case 'L':
+                    x--;break;
+            }
+        }
+        return x==0 && y==0; 
+    }
+}
