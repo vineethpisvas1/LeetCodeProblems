cf8f027969341b6f5db1790addd6a0cb349516fd
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-05-22 21:20:02 +0530

    Added solution to #500 Keyboard Row

--- Easy/KeyboardRow.java
+++ Easy/KeyboardRow.java
@@ -1,40 +1,51 @@
-import java.util.*;
-
-class KeyboardRow {
-	// Checks if the string can be typed from a single line in the keyboard.
-	public static boolean check(String a) {
-		a = a.toLowerCase();
-		char[] b = a.toCharArray();
-		int[] row = new int[b.length];
-		int[] hashrows = {2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
-		for(int i=0; i<b.length; i++) {
-			row[i] = hashrows[(int)b[i]-97];
-		}
-		int checker = 0;
-		for(int i=0; i<b.length; i++) {
-			if(row[i]!=row[0]) {
-				checker++;
-			}
-		}
-		if(checker>0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	// Main method.
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Give the size of the array: ");
-		int size = s.nextInt();
-		String[] a = new String[size];
-		for(int i=0; i<size; i++) {
-			a[i] = s.next();	
-		}
-		for(int i=0; i<size; i++) {
-			if(check(a[i])) {
-				System.out.print(a[i]+" ");
-			}
-		}
-	}
+class Solution {
+    public String[] findWords(String[] words) {
+        Map<Character, Integer> map = new HashMap<>();
+        String[] result = new String[words.length];
+        int count=0;
+        map.put('a', 2);
+        map.put('b', 3);
+        map.put('c', 3);
+        map.put('d', 2);
+        map.put('e', 1);
+        map.put('f', 2);
+        map.put('g', 2);
+        map.put('h', 2);
+        map.put('i', 1);
+        map.put('j', 2);
+        map.put('k', 2);
+        map.put('l', 2);
+        map.put('m', 3);
+        map.put('n', 3);
+        map.put('o', 1);
+        map.put('p', 1);
+        map.put('q', 1);
+        map.put('r', 1);
+        map.put('s', 2);
+        map.put('t', 1);
+        map.put('u', 1);
+        map.put('v', 3);
+        map.put('w', 1);
+        map.put('x', 3);
+        map.put('y', 1);
+        map.put('z', 3);
+        for(String word : words) {
+            String tempWord = word.toLowerCase();
+            int breakCount = 0;
+            for(char c : tempWord.toCharArray()) {
+                if(map.get(tempWord.charAt(0))!=map.get(c)) {
+                    breakCount++;
+                }
+            }
+            if(breakCount==0) {
+                result[count]=word;
+                count++;
+            }
+        }
+        String[] actualResult = new String[count];
+        for(int i=0; i<count; i++) {
+            actualResult[i]=result[i];
+        }
+        return actualResult;
+    }
 }
