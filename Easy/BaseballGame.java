7d517f7fbe171262a09434153978dea143bc5fdb
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-05-23 23:11:20 +0530

    Added solution to #682 Baseball Game

---
+++ Easy/BaseballGame.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] ops) {
+        List<Integer> list = new ArrayList<>();
+        for(String string : ops) {
+            if(string.equals("C")) {
+                if(list.size()>0) {
+                    list.remove(list.size()-1);
+                }
+            } else if(string.equals("D")) {
+                if(list.size()>0) {
+                    list.add(2*list.get(list.size()-1));
+                }
+            } else if(string.equals("+")) {
+                if(list.size()>1) {
+                    list.add(list.get(list.size()-1)+list.get(list.size()-2));
+                }
+            } else {
+                list.add(Integer.parseInt(string));
+            }
+        }
+        int sum=0;
+        for(int i=0; i<list.size(); i++) {
+            sum += list.get(i);
+        }
+        return sum;
+    }
+}
