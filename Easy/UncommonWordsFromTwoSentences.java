82a0b1ac84e67f4f35232630f5ae2a24e856531e
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-05-24 13:45:30 +0530

    Added solution to #884 Uncommon Words From Two Sentences

--- Easy/UncommonWordsFromTwoSentences.java
+++ Easy/UncommonWordsFromTwoSentences.java
@@ -5,32 +5,16 @@ class Solution {
         } else if(B.length()==0) {
             return A.split(" ");
         }
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
-        for(String s : A.split(" ")) {
-            map1.put(s, (map1.containsKey(s)?map1.get(s):0)+1);
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : (A+" "+B).split(" ")) {
+            map.put(s, (map.containsKey(s)?map.get(s):0)+1);
         }
-        for(String s : B.split(" ")) {
-            map2.put(s, (map2.containsKey(s)?map2.get(s):0)+1);
-        }
-        String[] resultTemp = new String[250];
-        int counter = 0;
-        for(String key : map1.keySet()) {
-            if(!map2.containsKey(key) && map1.get(key)==1) {
-                resultTemp[counter]=key;
-                counter++;
+        List<String> list = new ArrayList<>();
+        for(String key : map.keySet()) {
+            if(map.get(key)==1) {
+                list.add(key);
             }
         }
-        for(String key : map2.keySet()) {
-            if(!map1.containsKey(key) && map2.get(key)==1) {
-                resultTemp[counter]=key;
-                counter++;
-            }
-        }
-        String[] result = new String[counter];
-        for(int i=0; i<counter; i++) {
-            result[i] = resultTemp[i];
-        }
-        return result;
+        return list.toArray(new String[0]);
     }
 }
