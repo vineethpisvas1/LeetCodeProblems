763902eedc2c4226ee2bb532ed36d670fd40b138
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-05-30 23:27:04 +0530

    Added solution to #566 Reshape The Matrix

--- Easy/ReshapeTheMatrix.java
+++ Easy/ReshapeTheMatrix.java
@@ -1,7 +1,5 @@
-import java.util.*;
-
-class ReshapeTheMatrix {
-	public static int[][] reshape(int[][] a, int r, int c) {
+class Solution {
+	public int[][] reshape(int[][] a, int r, int c) {
 		int m = a.length;
 		int n = a[0].length;
 		int[][] b = new int[r][c];
@@ -22,35 +20,8 @@ class ReshapeTheMatrix {
 				}
 			}
 		} else {
-			System.out.println("Wrong Combination!!");
 			b = a;
 		}
 		return b;
-	}
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Give the rows of initial matrix: ");
-		int r1 = s.nextInt();
-		System.out.println("Give the columns of initial matrix: ");
-		int c1 = s.nextInt();
-		int[][] a = new int[r1][c1];
-		System.out.println("Give the initial matrix: ");	
-		for(int i=0; i<r1; i++) {
-			for(int j=0; j<c1; j++) {
-				a[i][j] = s.nextInt();
-			}
-		}
-		System.out.println("Give the rows of final matrix: ");
-		int r2 = s.nextInt();
-		System.out.println("Give the columns of final matrix: ");
-		int c2 = s.nextInt();
-		int[][] b = reshape(a, r2, c2);
-		System.out.println("The final matrix: \n");	
-		for(int i=0; i<r2; i++) {
-			for(int j=0; j<c2; j++) {
-				System.out.print(b[i][j]+" ");
-			}
-			System.out.println();
-		}
 	}
 }
