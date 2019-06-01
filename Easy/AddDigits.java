a03046e5c2e54c74384da88cd16d9a3d0de0c987
Vineeth Karanam <vineethpisvas1@gmail.com>
2019-06-01 22:24:34 +0530

    Added solution to #258 Add Digits

--- Easy/AddDigits.java
+++ Easy/AddDigits.java
@@ -6,19 +6,4 @@ class Solution {
         int result = num%9;
         return result==0?9:result;
     }
-    
-    /*
-    public int addDigits(int num) {
-        String number = Integer.toString(num);
-        int sum = 0;
-        while(number.length()>1) {
-            sum = 0;
-            for(char c : number.toCharArray()) {
-                sum += c - 48;
-            }
-            number = Integer.toString(sum);
-        }
-        return Integer.parseInt(number);
-    }
-    */
 }
